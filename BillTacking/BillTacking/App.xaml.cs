476b49eeee63858e564c43db4034fd956ead4201
depakmallick <depak.mallick@yahoo.com>
2023-01-19 17:34:36 -0500

    Initial commit

---
+++ BillTacking/BillTacking/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using BillTacking.Services;
+using BillTacking.Views;
+
+namespace BillTacking
+{
+    public partial class App : Application
+    {
+
+        public App ()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart ()
+        {
+        }
+
+        protected override void OnSleep ()
+        {
+        }
+
+        protected override void OnResume ()
+        {
+        }
+    }
+}
+
